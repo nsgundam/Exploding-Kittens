7a5b4b2660076761b6f2eb861e912a54eab64e69
Kittisak <Kittisaksupanutchaisakul@gmail.com>
2026-02-25 21:58:33 +0700

    fetchapi

--- frontend/app/Lobby/page.tsx
+++ frontend/app/Lobby/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import RoomCard from "../components/RoomCard";
 import JoinModal from "../components/JoinModal";
 import CreateRoomModal from "../components/CreateRoomModal";
